e828ad139aedfa6e9547ec80b9e18ef21c94213e
dnam-uet <51197563+dnam-uet@users.noreply.github.com>
2020-04-18 16:46:01 +0700

    Add button scroll to top

---
+++ js/header.js
@@ -0,0 +1,18 @@
+// Scroll to top button
+
+window.onscroll = function(){scrollFunction()};
+var buttonScroll = document.getElementById("scroll__top");
+
+buttonScroll.addEventListener("click",function(){
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
+
+function scrollFunction(){
+	if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+		buttonScroll.style.display = "block";
+	}
+	else{
+		buttonScroll.style.display = "none";
+	}
+}
